e81cd997dc848358200ba89683326b0088e5458f
Jihoon <dugubi1004@gmail.com>
2026-04-21 13:15:39 +0900

    빌드오류 수정

--- src/features/map/pages/MapHomeUI/MapHomeUI.tsx
+++ src/features/map/pages/MapHomeUI/MapHomeUI.tsx
@@ -80,7 +80,9 @@ export function MapHomeUI(props: MapHomeUIProps) {
     onMapReady,
     onViewportChange,
     createOpen,
+    editOpen,
     createPinKind,
+
     draftHeaderPrefill,
     selectedViewItem,
     prefillAddress,
